814fe7bf82e958937b125409990cf3e51ae00719
Marius van Staden <mhvstaden91@gmail.com>
2019-05-18 18:49:35 +0200

    Ui Update

---
+++ app/src/main/java/com/example/aquaponics_project/Connections.java
@@ -0,0 +1,16 @@
+package com.example.aquaponics_project;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class Connections extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.connections);
+    }
+}
